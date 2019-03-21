a746c7bf53f14e51e8810a7fb11cfc68b3fa0cdc
= Xuan <=tienxuantt@gmail.com>
2019-03-21 07:48:59 +0700

    ăeffwe

--- ChatApp/Migrations/201903201642253_CreateDB.Designer.cs
+++ ChatApp/Migrations/201903201642253_CreateDB.Designer.cs
@@ -7,13 +7,13 @@ namespace ChatApp.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class addDb : IMigrationMetadata
+    public sealed partial class CreateDB : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(addDb));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(CreateDB));
         
         string IMigrationMetadata.Id
         {
-            get { return "201903200651575_addDb"; }
+            get { return "201903201642253_CreateDB"; }
         }
         
         string IMigrationMetadata.Source

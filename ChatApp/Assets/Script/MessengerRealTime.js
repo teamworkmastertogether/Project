aec67c49e086ffe2a7c0471efa1d83a4510153bb
QuangNX <adquang199x@gmail.com>
2019-03-28 00:49:16 +0700

    ádd

--- ChatApp/Assets/Script/MessengerRealTime.js
+++ ChatApp/Assets/Script/MessengerRealTime.js
@@ -528,38 +528,6 @@ $("#Cancel").click(function () {
     $('#Password').css("border-color", "#ccc");
 
 });
-/*$("#txtKeyword").autocomplete({
-    minLength: 0,
-    source: function (request, response) {
-        $.ajax({
-            url: "/Home/Search",
-            dataType: "json",
-            data: {
-                keyword: request.term
-            },
-            success: function (data) {
-                response(data.data);
-            }
-        });
-    },
-    focus: function (event, ui) {
-        $("#txtKeyword").val(ui.item.label);
-        return false;
-    },
-    select: function (event, ui) {
-        $("#txtKeyword").val(ui.item.label);
-        //$("#project-id").val(ui.item.value);
-        //$("#project-description").html(ui.item.desc);
-        //$("#project-icon").attr("src", "images/" + ui.item.icon);
- 
-        return false;
-    }
-})
-    .autocomplete("instance")._renderItem = function (ul, item) {
-        return $("<li>")
-            .append("<div class='getValueId'>" + item.label + /*"<br>" + item.desc + "</div>")
-            .appendTo(ul);
-    };*/
 $("#ChangePassword").click(function () {
     $("#curPassword").hide();
     var html = '';
